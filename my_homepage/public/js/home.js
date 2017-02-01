030d87e881f4e2fd4c6639d57ee45199b6267ead
BoogieJay <jw4100@nyu.edu>
2017-02-01 10:18:03 -0500

    finsh project and footer

---
+++ my_homepage/public/js/home.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var slider = $('#lightSlider').lightSlider({
+        gallery:true,
+        item:1,
+        vertical:true,
+        vThumbWidth:200,
+        thumbItem:4,
+        thumbMargin:0,
+        slideMargin:100,
+        loop:false,
+        speed: 350,
+        controls:false
+    });
+
+});
